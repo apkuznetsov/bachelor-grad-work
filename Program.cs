34667a47c634b875d9860b5e78ba17ae969741ff
Andrey <kouznts@ya.ru>
2020-05-10 13:59:37 +0400

    testing Postgres connection

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Webapp;
 
 namespace Webapp
 {
@@ -13,7 +14,25 @@ namespace Webapp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            using (webappdbContext db = new webappdbContext())
+            {
+      
+                Users user1 = new Users { Name = "Tom", Age = 33 };
+                Users user2 = new Users { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Users list:");
+                foreach (Users u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
