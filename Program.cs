b255ab5a09b2698915b012862bf865e0149e4de0
Andrey <kouznts@ya.ru>
2020-05-10 16:47:54 +0400

    delete Postgres connection testing

--- Program.cs
+++ Program.cs
@@ -14,25 +14,7 @@ namespace Webapp
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
-            using (webappdbContext db = new webappdbContext())
-            {
-      
-                Users user1 = new Users { Name = "Tom", Age = 33 };
-                Users user2 = new Users { Name = "Alice", Age = 26 };
-
-                db.Users.Add(user1);
-                db.Users.Add(user2);
-                db.SaveChanges();
-
-                var users = db.Users.ToList();
-                Console.WriteLine("Users list:");
-                foreach (Users u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-                }
-            }
-            Console.Read();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
