e59b74776b3d3daa1d07918d922d58e9e8ddda83
Andrey <kouznts@ya.ru>
2020-05-16 16:23:58 +0400

    add display name for sensors columns

--- WebappDb/Models/Sensors.cs
+++ WebappDb/Models/Sensors.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebappDb
 {
@@ -12,16 +13,27 @@ namespace WebappDb
         }
 
         public int SensorId { get; set; }
-        public string Metadata { get; set; }
-        public int DataTypeId { get; set; }
-        public string IpAddress { get; set; }
-        public int Port { get; set; }
-        public int CommunicationProtocolId { get; set; }
 
+        [Display(Name = "Название")]
         public string Name { get; set; }
 
-        public virtual CommunicationProtocols CommunicationProtocol { get; set; }
+        [Display(Name = "Описание")]
+        public string Metadata { get; set; }
+
+        [Display(Name = "JSON-схема фиксируемых данных")]
+        public int DataTypeId { get; set; }
+
+        [Display(Name = "Сетевой протокол")]
+        public int CommunicationProtocolId { get; set; }
+
+        [Display(Name = "IP-адрес")]
+        public string IpAddress { get; set; }
+
+        [Display(Name = "Порт")]
+        public int Port { get; set; }
+
         public virtual Datatypes DataType { get; set; }
+        public virtual CommunicationProtocols CommunicationProtocol { get; set; }
         public virtual ICollection<ExperimentSensors> ExperimentSensors { get; set; }
         public virtual ICollection<ProcessingSensors> ProcessingSensors { get; set; }
     }
