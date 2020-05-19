e652ae0d590483619c2ed653d1a5933942ce41c7
Andrey <kouznts@ya.ru>
2020-05-19 21:26:11 +0400

    create experiment sensor view model

--- WebappDb/Models/ExperimentSensors.cs
+++ WebappDb/Models/ExperimentSensors.cs
@@ -2,8 +2,8 @@
 {
     public partial class ExperimentSensors
     {
-        public int ExperimentId { get; set; }
         public int ExperimentSensorId { get; set; }
+        public int ExperimentId { get; set; }
         public int SensorId { get; set; }
 
         public virtual Experiments Experiment { get; set; }
