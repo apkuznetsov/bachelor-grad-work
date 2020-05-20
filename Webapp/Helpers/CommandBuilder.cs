07e3c9234dc59bea1d25adeeead28176ed3e62e3
Andrey <kouznts@ya.ru>
2020-05-20 22:42:40 +0400

    create sensor listener start command build method

---
+++ Webapp/Helpers/CommandBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace Webapp.Helpers
+{
+    public static class CommandBuilder
+    {
+        public static string BuildSensorListenerStartCommand(int testId, string sensorIpAddress, int sensorPort, int durationSeconds)
+        {
+            return $"-testId {testId} -executionTime {durationSeconds} -sensors {sensorIpAddress}:{sensorPort}";
+        }
+    }
+}
