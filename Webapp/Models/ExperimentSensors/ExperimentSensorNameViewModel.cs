fd7b0c27a41d98960016925fc07cffb83f116826
Andrey <kouznts@ya.ru>
2020-05-20 10:32:56 +0400

    create experiment sensor name view model

---
+++ Webapp/Models/ExperimentSensors/ExperimentSensorNameViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Webapp.Models.ExperimentSensors
+{
+    public class ExperimentSensorNameViewModel
+    {
+        public int ExperimentSensorId { get; set; }
+
+        public int ExperimentId { get; set; }
+
+        public int SensorId { get; set; }
+
+        public string SensorName { get; set; }
+    }
+}
