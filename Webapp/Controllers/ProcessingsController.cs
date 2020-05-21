a0c07bd2b96c5dbca95912f694868c135154cc90
Andrey <kouznts@ya.ru>
2020-05-21 21:10:30 +0400

    create processings index method

--- Webapp/Controllers/ProcessingsController.cs
+++ Webapp/Controllers/ProcessingsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Webapp.Models;
 using WebappDb;
 
 namespace Webapp.Controllers
@@ -10,6 +12,19 @@ namespace Webapp.Controllers
         public ProcessingsController(WebappdbContext context)
         {
             db = context;
+        }
+
+        // GET: Processings
+        public IActionResult Index()
+        {
+            var processings = db.Processings.Select(m => new ProcessingNameViewModel { ProcessingId = m.ProcessingId, Name = m.Name }).ToList();
+
+            if (processings == null)
+            {
+                return NotFound();
+            }
+
+            return View(processings);
         }
     }
 }
