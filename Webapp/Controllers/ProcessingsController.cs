ce5781a8c940214ea6abdab610db3cb48422c21a
Andrey <kouznts@ya.ru>
2020-05-21 20:44:30 +0400

    create processings controller

---
+++ Webapp/Controllers/ProcessingsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebappDb;
+
+namespace Webapp.Controllers
+{
+    public class ProcessingsController : Controller
+    {
+        private readonly WebappdbContext db;
+
+        public ProcessingsController(WebappdbContext context)
+        {
+            db = context;
+        }
+    }
+}
