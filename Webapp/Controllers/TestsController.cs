4c85109a71c5bde64a06a720624fee64aaef1fb1
Andrey <kouznts@ya.ru>
2020-05-21 08:12:32 +0400

    add SensorListener project reference

--- Webapp/Controllers/TestsController.cs
+++ Webapp/Controllers/TestsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using System;
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Webapp.Models;
 using Webapp.Models.Tests;
 using WebappDb;
+using SensorListener;
 
 namespace Webapp.Controllers
 {
